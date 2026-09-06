3a9a4c63b8f4d082c3e8c1452939a43c9acc10a6
Lakshita200 <swetalakshita@gmail.com>
2026-09-06 11:41:35 +0800

    Update and rename README.md to README.m

--- README.m
+++ README.m
@@ -4,12 +4,21 @@ I am a Year 2 student with hands - on experience in UI/UX, software Developer, F
 My Primary interest is in UI/UX.
 
 
-
 Academic projects is a collection of my coursework and projects from Nanyang Technological University (NTU).
 _______________________________________________________________________________________________________________________________________________________________________________________________________________________________
 # PROJECTS  
 # Academic Projects
-https://github.com/Lakshita200/Sc2008
+# Year 1
+SC1003- 
+sc1007 - 
+Sc1008 - 
+Sc2002 - https://github.com/Lakshita200/oopd-proj
+# Year 2
+Sc2006 - https://github.com/Lakshita200/SpendWise
+Sc2008 - https://github.com/Lakshita200/Sc2008
+
+
+
 _______________________________________________________________________________________________________________________________________________________________________________________________________________________________
 # Core Skills
 
